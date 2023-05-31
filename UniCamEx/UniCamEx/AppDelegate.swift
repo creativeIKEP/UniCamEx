68e98248f5ec22e23144cf79639d723438b433b3
IKEP <34697515+creativeIKEP@users.noreply.github.com>
2023-05-31 21:22:47 +0900

    add Game sample app with swift

---
+++ UniCamEx/UniCamEx/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  UniCamEx
+//
+//  Created by IKEP on 2023/05/31.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
